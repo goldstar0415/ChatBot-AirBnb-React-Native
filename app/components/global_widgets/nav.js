0e99a75ea751f573cb9e86200556e5c9c87b9c5b
goldstar0415 <topdeveloper1212@gmail.com>
2016-08-05 15:52:43 -0500

    Working external Nav

--- app/components/global_widgets/nav.js
+++ app/components/global_widgets/nav.js
@@ -1,15 +1,17 @@
 
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Nav extends Component {
 
   render() {
     return (
+    	<TouchableOpacity onPress ={this.props.onPress}>
       <View style={{padding:15}}>
       <Icon color={this.props.color} name="menu" size={25} />
       </View>
+      </TouchableOpacity>
     );
   }
 }
